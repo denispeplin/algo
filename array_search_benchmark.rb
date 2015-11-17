693535dcf99562d733d0282f6a1632d888774b49
denispeplin <denis.peplin@gmail.com>
2015-11-17 08:51:20 +0300

    Run quickserch 100000 times to get non-zero results.

--- array_search_benchmark.rb
+++ array_search_benchmark.rb
@@ -1,14 +1,16 @@
 require 'benchmark'
 require './array_search'
 
-GC.disable
+k_10 = 10_000_000
+k_100 = 100_000_000
 
-k = 100_000_000
-
-vacant_search = nil
+vacant_search_10 = nil
+vacant_search_100 = nil
 
 Benchmark.bmbm do |x|
-  x.report(:fill_in) { vacant_search = VacantSearch.new(k) }
-  x.report(:sequental_search) { vacant_search.sequental_search }
-  x.report(:quicksearch) { vacant_search.quicksearch }
+  x.report(:fill_in_10M) { vacant_search_10 = VacantSearch.new(k_10) }
+  x.report(:fill_in_100M) { vacant_search_100 = VacantSearch.new(k_100) }
+  x.report(:sequental_search_10M) { vacant_search_10.sequental_search }
+  x.report(:sequental_search_100M) { vacant_search_100.sequental_search }
+  x.report(:quicksearch_100Mx100K) { 100_000.times { vacant_search_100.quicksearch } }
 end
